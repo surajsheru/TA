1c4f4956d8eb25516448e5533ac6741eab7e71a4
suraj kamal.shaik <surajsheru@gmail.com>
2020-09-28 11:27:58 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Check from './components/Check';
+
+function App() {
+  return (
+    <div className="App">
+      <Check/>
+    </div>
+  );
+}
+
+export default App;
