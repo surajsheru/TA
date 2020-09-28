289ad648db7d8afa06ec98afd2c454d6e18b7412
suraj kamal.shaik <surajsheru@gmail.com>
2020-09-28 11:58:56 +0530

    Updates

---
+++ precache-manifest.bba29ac430cfddc56344bf0a29888717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d6a3f7036ab559b7a2da961bdeb55b",
+    "url": "/TA/index.html"
+  },
+  {
+    "revision": "e3e907f2456823ebfe92",
+    "url": "/TA/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0e05df17384da4a2bf83",
+    "url": "/TA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3e907f2456823ebfe92",
+    "url": "/TA/static/js/2.5470efc5.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/TA/static/js/2.5470efc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e05df17384da4a2bf83",
+    "url": "/TA/static/js/main.17fb254b.chunk.js"
+  },
+  {
+    "revision": "66ec2885b2788ca336c1",
+    "url": "/TA/static/js/runtime-main.6e284010.js"
+  }
+]);
